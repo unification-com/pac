5db0e6ec3723e974d36b7ab1dc2d1b43632fda9f
Codegnosis <paul@codegnosis.co.uk>
2020-07-02 15:33:50 +0100

    victim component styling

--- src/ui/components/report/victim.js
+++ src/ui/components/report/victim.js
@@ -1,32 +1,42 @@
+import utilStyles from "../../styles/utils.module.css";
+
 export default function Victim({ name, race, gender, age, image = '' }) {
     if(name === '') {
         return <div></div>
     } else {
-        return <div>
-            <h5>Victim Info</h5>
+        return <div className={[utilStyles.section, utilStyles.innerSection].join(' ')}>
+            <h4 className={utilStyles.headingXs}>Victim Info</h4>
+
+            <div>
             {
                 (image !== '') ?
                     <img src={image} /> :
                     <></>
             }
-            <ul>
-                <li key='victim_name'>
-                    <strong>Name:</strong> {name}
+            <ul className={utilStyles.nb}>
+                <li className={utilStyles.liFloat} key='victim_name'>
+                    <label className={utilStyles.label}>Name</label>
+                    <span className={[utilStyles.input, utilStyles.spanField].join(' ')}>{name}</span>
                 </li>
-                <li key='victim_race'>
-                    <strong>Race:</strong> {race}
+                <li className={utilStyles.liFloat} key='victim_race'>
+                    <label className={utilStyles.label}>Race</label>
+                    <span className={[utilStyles.input, utilStyles.spanField].join(' ')}>{race}</span>
                 </li>
                 {
                     (parseInt(age) > 0) ?
-                        <li key='victim_age'>
-                            <strong>Age:</strong> {age}
+                        <li className={utilStyles.liFloat} key='victim_age'>
+                            <label className={utilStyles.label}>Age</label>
+                            <span className={[utilStyles.input, utilStyles.spanField].join(' ')}>{age}</span>
                         </li> :
                         <></>
                 }
-                <li key='victim_gender'>
-                    <strong>Gender:</strong> {gender}
+                <li className={utilStyles.liFloat} key='victim_gender'>
+                    <label className={utilStyles.label}>Gender</label>
+                    <span className={[utilStyles.input, utilStyles.spanField].join(' ')}>{gender}</span>
                 </li>
+                <li className={utilStyles.clearLeft}></li>
             </ul>
+            </div>
         </div>
     }
 }

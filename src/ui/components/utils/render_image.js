04b46a562477f9b23ea52a18ebd8871e824b52ba
Codegnosis <paul@codegnosis.co.uk>
2020-07-03 16:22:58 +0100

    fix empty fallback img

--- src/ui/components/utils/render_image.js
+++ src/ui/components/utils/render_image.js
@@ -1,4 +1,9 @@
 export default function RenderImage(image, fallbackImage = '/assets/img/logo.png', showFallback = true) {
+
+    if(fallbackImage === '' || fallbackImage === null) {
+        fallbackImage = '/assets/img/logo.png'
+    }
+
     const onerror = (showFallback) ?
         `this.onerror=null;this.src=this.dataset.fallbackImage;this.width=300;this.height=300` :
         `this.onerror=null;this.style.display=none`

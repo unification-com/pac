b21f948117a0b46828ca8f4c44e79f64f7316358
Codegnosis <paul@codegnosis.co.uk>
2020-07-06 13:01:52 +0100

    display 1x1 img is no fallback is passed

--- src/ui/components/utils/render_image.js
+++ src/ui/components/utils/render_image.js
@@ -1,12 +1,13 @@
-export default function RenderImage(image, fallbackImage = '/assets/img/logo.png', showFallback = true) {
+export default function RenderImage(image,
+                                    fallbackImage = '/assets/img/logo.png', showFallback = true) {
 
-    if(fallbackImage === '' || fallbackImage === null) {
+    if((fallbackImage === '' || fallbackImage === null) && showFallback) {
         fallbackImage = '/assets/img/logo.png'
     }
 
     const onerror = (showFallback) ?
         `this.onerror=null;this.src=this.dataset.fallbackImage;this.width=300;this.height=300` :
-        `this.onerror=null;this.style.display=none`
+        `this.onerror=null;this.src=/assets/img/1x1.png;this.width=1;this.height=1`
 
     return (
     <div dangerouslySetInnerHTML={{

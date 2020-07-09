121e325ce8dfa0c6210eb2875c5eca981673b66d
Codegnosis <paul@codegnosis.co.uk>
2020-07-09 12:29:14 +0100

    stringify JSON

--- src/ui/components/report/beacon.js
+++ src/ui/components/report/beacon.js
@@ -38,7 +38,7 @@ export default function Beacon({beaconHash, beaconTimestampId, beaconTimestamp, 
                     Public Key Type: {beaconTx.tx.value.signatures[0].pub_key.type}<br />
                     Public Key: {beaconTx.tx.value.signatures[0].pub_key.value}<br />
                     Signature: {beaconTx.tx.value.signatures[0].signature} <br/>
-                    Raw Tx data: <textarea defaultValue={beaconTx.tx}/>
+                    Raw Tx data: <textarea defaultValue={JSON.stringify(beaconTx.tx)}/>
                 </p>
             </div>
         </>

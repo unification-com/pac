272b1349baaf72493855714a8899223244fe0d95
Codegnosis <paul@codegnosis.co.uk>
2020-07-10 15:08:25 +0100

    ui - api endpoint to get IPFS dbbackups

---
+++ src/ui/pages/api/ipfs.js
@@ -0,0 +1,18 @@
+import nextConnect from 'next-connect';
+import middleware from '../../middleware/database';
+const handler = nextConnect();
+
+const PAC_CONFIG = require('../../../common/constants');
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+    let data = await req.db.collection(PAC_CONFIG.IPFS_HISTORY_COLLECTION)
+        .find({type: 'db_backup'})
+        .sort({ timestamp: -1 })
+        .limit(10).toArray();
+
+    res.json(data);
+});
+
+export default handler;

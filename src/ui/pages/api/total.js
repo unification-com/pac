f4709bfddecbcd8d451dfc6b1c1420986fe3c39a
Codegnosis <paul@codegnosis.co.uk>
2020-07-10 15:05:30 +0100

    ui - total api, get collection name from config object

--- src/ui/pages/api/total.js
+++ src/ui/pages/api/total.js
@@ -2,10 +2,12 @@ import nextConnect from 'next-connect';
 import middleware from '../../middleware/database';
 const handler = nextConnect();
 
+const PAC_CONFIG = require('../../../common/constants');
+
 handler.use(middleware);
 
 handler.get(async (req, res) => {
-    res.json(await req.db.collection('incident_reports').find().count());
+    res.json(await req.db.collection(PAC_CONFIG.INCIDENT_REPORT_COLLECTION).find().count());
 });
 
 export default handler;
